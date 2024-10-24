88ddecb20e3939cfa408450b5f93c8629cedab05
LeuleY <leulemerso2003@gmail.com>
2024-10-24 11:42:29 -0400

    img and login center

--- src/Login.js
+++ src/Login.js
@@ -3,13 +3,22 @@ import './Login.css';
 
 const Login = () => {
   return (
-    <div className="login">
+    <div className="centerbox">
+
+      <div className='loginbox'>
+          <form className='form'>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button type="submit">Login</button>
+          </form>
+
+      </div>
+
+      <div className='registerbox'>
+
+      </div>
       
-      <form>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+     
     </div>
   );
 };
