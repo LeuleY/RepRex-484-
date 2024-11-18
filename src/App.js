435796ed11e3a207c612b59edfa509eb7cc90cab
LeuleY <leulemerso2003@gmail.com>
2024-11-18 16:41:32 -0500

    uncomment

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import VideoList from './Components/VideoList';
 import Calculator from './Components/Calculator'
 import About from './Components/About';
 import Community from './Components/Community';
-import HomePage from './Components/HomePage';
+import HomePage from './Components/HomePage.jsx';
 
 
 
@@ -19,13 +19,14 @@ function App() {
   return (
     <div >
    
-        {/* <Routes>
-          <Route index element={<Login/>}/> 
+        <Routes>
+          {/* <Route index element={<Login/>}/>  */}
+          <Route path='/' element={<HomePage/>} />
           <Route path='/login' element={<Login/>} />
           <Route path='/register' element={<Register/>} />
           <Route path='/details/:exercise/' element={<DetailsPage/>}/>
           <Route path='/cards' element={<CardList/>}/>
-        </Routes> */}
+        </Routes>
    
    
    <HomePage></HomePage>
