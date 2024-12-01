4e6559ee16edbae251bed21d61a4c13c7bea294e
LeuleY <leulemerso2003@gmail.com>
2024-12-01 14:29:04 -0500

    testing

--- server/routes/workoutRoutes.js
+++ server/routes/workoutRoutes.js
@@ -8,4 +8,9 @@ router.post('/createCardio', newWorkoutCardio);
 router.post('/deleteWorkout', deleteWorkout);
 router.post('/getWorkouts', getWorkouts);
 
+// Sample route for workouts
+router.get('/', (req, res) => {
+    res.send('Workout routes are working');
+  });
+
 module.exports = router;
